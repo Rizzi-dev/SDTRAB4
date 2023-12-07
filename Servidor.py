018b75ce3c3c1d168b923a9b970987b2b66397b6
Gabriel Rizzi <75540693+Rizzi-dev@users.noreply.github.com>
2023-12-07 16:12:48 -0300

    Update Servidor.py

--- Servidor.py
+++ Servidor.py
@@ -3,65 +3,82 @@ from flask_cors import CORS
 import json
 import datetime
 from time import sleep
-
-
-# Criação da instância da aplicação Flask e habilitação do CORS
-app = Flask("Servidor")
+ 
+app = Flask(__name__)
 CORS(app)
 
-#Classe que represeta o gerenciamento de Produto
+users = {}  # Dicionário de usuários (nome do usuário -> objeto do usuário)
+products = {}  # Dicionário de produtos (código do produto -> objeto do produto)
+
+# Representa o produto
 class Product:
-    def __init__(self, code, name, description, quantity, price, min_stock):
-        #Inicialização dos atributos do produto
-        self.code = code
-        self.name = name
-        self.description = description
-        self.quantity = quantity
-        self.price = price
-        self.min_stock = min_stock
-        self.movements = []
+    def __init__(self, code, name, description, quantity, price, minStock):
+            self.code = code
+            self.name = name
+            self.description = description
+            self.quantity = quantity
+            self.price = price
+            self.minStock = minStock
+            self.movements = []
 
     def add_entry(self, quantity):
-        #Adiciona uma entrada de produto ao estoque
-        self.quantity = quantity
-        self.movements.append((datetime.datetime.now(), "entrada", quantity))
+            self.quantity += quantity
+            self.movements.append((datetime.datetime.now(), "entrada", quantity))
 
     def add_exit(self, quantity):
-        #Adiciona uma saída de produto do estoque, se a quantidade disponível for suficiente
-        if self.quantity >= quantity:
-            self.quantity -= quantity
-            self.movements.append((datetime.datetime.now(), "saída", quantity))
+            if self.quantity >= quantity:
+                self.quantity -= quantity
+                self.movements.append((datetime.datetime.now(), "saída", quantity))
 
     def get_stock_status(self):
-        #Retorna o status do estoque do produto em um formato JSON
-        return {
-            "code": self.code,
-            "name": self.name,
-            "description": self.description,
-            "quantity": self.quantity,
-            "price": self.price,
-            "min_stock": self.min_stock,
-        }
-    
-
+            return {
+                "code": self.code,
+                "name": self.name,
+                "description": self.description,
+                "quantity": self.quantity,
+                "price": self.price,
+                "minStock": self.minStock,
+            }
 
 # Classe que representa um usuário do sistema
 class User:
     def __init__(self, name):
         self.name = name
+  
 
-#Dicionários para armazenar usuários e produtos
-users={}
-products={}
 
-# Classe de gerenciamento do Estoque
+@app.route('/api/products/exit/<string:productId>', methods=['POST'])
+def record_exit(productId):
+    data = request.json
+    quantityToSubtract = data.get('quantityToSubtract')
+ 
+    response = ""
+    notify = ""
+    if productId in products:
+        product = products[productId]
+
+        if quantityToSubtract > 0 and quantityToSubtract <= product.quantity:
+            product.add_exit(quantityToSubtract)
+            response = "Produto removido do estoque."
+            if product.quantity < product.minStock:
+                 notify="Quantidade abaixo do estoque mínimo!"
+        else:
+            response = "Quantidade inválida para remoção."
+
+    else:
+        response = "Produto não encontrado."
+
+    return jsonify({"message": response, "notify":notify})
+
+
+
+# Funcoes do Sistema de gestão de estoque
 @app.route('/api/register', methods=['POST'])
 def register_user():
     data = request.json
     name = data.get('name')
     response = ""
     if name not in users:
-        #Cria um novo usuário e o adiciona ao dicionário de usuários
         user = User(name)
         users[name] = user
         response = f"Usuário {name} registrado com sucesso."
@@ -71,69 +88,46 @@ def register_user():
     return jsonify({"message": response})
 
 
-
-#Rota para registrar um novo produto
 @app.route('/api/products/register', methods=['POST'])
 def register_product():
     data = request.json
     data = data["data"]
     code = data.get('code')
 
-    name = data.get('name')
-    description = data.get('description')
-    price = data.get('price')
-    min_stock = data.get('min_stock')
+    # Verifique se já existe um produto com o mesmo código
+    if code in products:
+        response = "Já existe um produto com o código fornecido."
+    else:
+        name = data.get('name')
+        description = data.get('description')
+        price = data.get('price')
+        minStock = data.get('minStock')
 
-    #Cria uma nova instância de produto e o adiciona ao dicionário de produtos
-    product = Product(code, name, description, min_stock, price, min_stock)
-    products[code] = product
+        # Crie um objeto Product com as informações e adicione-o ao dicionário de produtos
+        product = Product(code, name, description, minStock, price, minStock)
+        products[code] = product
 
-    response = "Produto registrado com sucesso!"
+        response = "Produto registrado com sucesso"
 
     return jsonify({"message": response})
 
 
-#Rota para registrar uma entrada de produto ao estoque
 @app.route('/api/products/entry/<string:productId>', methods=['POST'])
 def record_entry(productId):
     data = request.json
     quantity = data.get('quantityToAdd')
-
+        
     response = ""
     if productId in products:
-        #Adiciona uma entrada de produto ao estoque 
-        response = "Produto adicionado"
+        response = "Produto adicionado ao estoque."
         product = products[productId]
         product.add_entry(quantity)
     else:
-        response = "Código não encontrado"
+        response = "Não há nenhum produto com esse código"
        
     return jsonify({"message": response})
 
 
-#Rota para registrar uma saída de produto do estoque
-@app.route('/api/products/exit/<string:productId>', methods=['POST'])
-def record_exit(productId):
-    data = request.json
-    quantityToSubtract = data.get('quantityToSubtract')
- 
-    response = ""
-    if productId in products:
-         #Adiciona uma saída de produto do estoque, se a quantidade disponível for suficiente
-        product = products[productId]
-
-        if quantityToSubtract > 0 and quantityToSubtract <= product.quantity:
-            product.add_exit(quantityToSubtract)
-            response = "Produto removido do estoque."
-        else:
-            response = "Não foi possível remover este produto."
-
-    else:
-        response = "Produto não encontrado."
-
-    return jsonify({"message": response})
-
-#Rota para obter a lista de usuários
 @app.route('/api/products', methods=['GET'])
 def get_products():
     product_list = []
@@ -142,7 +136,6 @@ def get_products():
     return jsonify(product_list)
 
 
-#Rota para obter a lista de usuários
 @app.route('/api/clients', methods=['GET'])
 def get_users():
     username_list = []
@@ -152,7 +145,6 @@ def get_users():
     return jsonify(username_list)
 
 
-#Rota para obter relatórios de movimentação de estoque dentro de um período específico
 @app.route('/api/reports/movements/<int:date>', methods=['GET'])
 def get_movement_reports(date):
     current_time = datetime.datetime.now()
@@ -173,7 +165,7 @@ def get_movement_reports(date):
                     product_info["movements"].append({
                          "time": movement_time,
                          "type": movement_type,
-                         "quantity": movement_quantity
+                        "quantity": movement_quantity
                             })
 
         fluxoMov.append(product_info)
@@ -182,7 +174,6 @@ def get_movement_reports(date):
     return jsonify(fluxoMov)
 
 
-#Rota para obter relatórios de produtos não vendidos dentro de um período específico
 @app.route('/api/reports/not-sold-since/<int:date>', methods=['GET'])
 def get_not_sold_reports(date):
 
@@ -208,7 +199,6 @@ def get_not_sold_reports(date):
     return jsonify(unsold_products)
 
 
-# Função para verificar produtos não vendidos e produtos com baixo estoque nos últimos 2 minutos
 def check_unsold_lowstock():
     current_time = datetime.datetime.now()
     time_ago = current_time - datetime.timedelta(minutes=2)
@@ -229,7 +219,7 @@ def check_unsold_lowstock():
                  "name": product.name
                     })
 
-        if product.quantity < product.min_stock:
+        if product.quantity < product.minStock:
             low_stock_products.append({
                  "code": product.code,
                  "name": product.name
@@ -242,14 +232,11 @@ def check_unsold_lowstock():
 
     return combined_data
 
-
-#Rota do SSE
+ 
 @app.route('/sse')
 def sse_demo():
     def check():
-        #Pausa a execução por 30 segundos
         sleep(30)
-        #Chama a função para verificar produtos não vendidos e com baixo estoque
         unsold_products = check_unsold_lowstock()
         return "data: {}\n\n".format(json.dumps(unsold_products))
 
@@ -259,7 +246,6 @@ def sse_demo():
     )
 
 
-#Inicia a aplicação Flask em modo debug
-app.run(debug=True)
-
-
+if __name__ == '__main__':
+    app.run(debug=True)
+    sleep(3000)
